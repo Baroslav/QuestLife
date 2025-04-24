4117942010798a4265f1b26cf2246700a5a7d600
Bogdan <barokurnq@gmail.com>
2025-04-24 23:50:24 +0300

    fiter refactoring

--- src/app/components/ui/achivementFilter/achivementFilter.tsx
+++ src/app/components/ui/achivementFilter/achivementFilter.tsx
@@ -1,11 +1,14 @@
+"use client"
+
+import { useState } from "react"
 import AchivemetDifficultItems from "./achivemetntFilterItems"
 import styles from "./style.module.css"
 
 const AchivementFilter = () => {
+
     return (
-        <div>
-            <p></p>
-            <img src="" alt="" />
+        <div className={styles.filter}>
+            <p>Achievement filter</p>
             <AchivemetDifficultItems/>
         </div>
     )
--- src/app/components/ui/achivementFilter/achivemetntFilterItems.tsx
+++ src/app/components/ui/achivementFilter/achivemetntFilterItems.tsx
@@ -1,32 +1,43 @@
+"use client"
+import { useState } from "react"
 import styles from "./style.module.css"
 
-type TypeOptions = "difficultLevel" | "complided" | "rejected"  | "acepded"
+
+type TypeOptions = "difficultLevel" | "status"
 
 type OptionsType = {
     type : TypeOptions , 
-    difficult : string, 
+    filtering : string, 
     color : string
 }
 
+type Filtering = {
+    filter : string
+}
+
 const options:OptionsType[] = [
-    {type : "difficultLevel" , difficult : "Legendary" , color : "249, 228, 0"},
-    {type : "difficultLevel" ,difficult : "Mythical" , color : "92, 3, 224"},
-    {type : "difficultLevel" , difficult : "Epic" , color : "157, 45, 45"},
-    {type : "difficultLevel" ,difficult : "Unique" , color : "35, 176, 40"},
-    {type : "difficultLevel" , difficult : "Default" , color : "222, 222, 222"},
-    {type : "complided" , difficult : "Default" , color : "222, 222, 222"},
-    {type : "rejected" , difficult : "Default" , color : "222, 222, 222"},
-    {type : "acepded" , difficult : "Default" , color : "222, 222, 222"},
+    {type : "difficultLevel" , filtering : "Legendary" , color : "249, 228, 0"},
+    {type : "difficultLevel" , filtering : "Mythical" , color : "92, 3, 224"},
+    {type : "difficultLevel" , filtering : "Epic" , color : "157, 45, 45"},
+    {type : "difficultLevel" , filtering : "Unique" , color : "35, 176, 40"},
+    {type : "difficultLevel" , filtering : "Default" , color : "222, 222, 222"},
+    {type : "status" , filtering: "complided" , color : "222, 222, 222"},
+    {type : "status" , filtering : "rejected" , color : "222, 222, 222"},
+    {type : "status" , filtering : "acepded" , color : "222, 222, 222"},
 ]
 
 const AchivemetDifficultItems = () => {
-    
+    const [actived , setActived] = useState<Filtering[]>([])
+
     return (
         <div className = {styles.filterItems}>
             {
-                options.map(({type , difficult , color}) => (
-                    <div className = {styles.filterItem} style = {{backgroundColor : `rgb(${color})`}}>
-                        <img src={`${type}.png`} alt="" />
+                options.map(({type , filtering , color} , i ) => (
+                    <div 
+                    key={type + filtering + color} 
+                    className = {styles.filterItem} 
+                    style = {{backgroundColor : `rgb(${color})`}}>
+                        <img src={type == "status" ? `${filtering}.png` : ``} alt="" />
                     </div>
                 ))
             }
