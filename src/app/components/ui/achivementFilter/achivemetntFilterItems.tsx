55e5bc15669de27e023f103df5563068b49e5b1e
Bogdan <barokurnq@gmail.com>
2025-05-06 21:22:43 +0300

    refactor Achivement filter

--- src/app/components/ui/achivementFilter/achivemetntFilterItems.tsx
+++ src/app/components/ui/achivementFilter/achivemetntFilterItems.tsx
@@ -3,11 +3,11 @@ import { useState } from "react"
 import styles from "./style.module.css"
 
 
-type TypeOptions = "difficultLevel" | "status"
+type TypeOptions = "difficultLevel" | "status" 
 
 type OptionsType = {
     type : TypeOptions , 
-    filtering : string, 
+    filtering : string | null, 
     color : string
 }
 
@@ -15,29 +15,45 @@ type Filtering = {
     filter : string
 }
 
+
 const options:OptionsType[] = [
-    {type : "difficultLevel" , filtering : "Legendary" , color : "249, 228, 0"},
-    {type : "difficultLevel" , filtering : "Mythical" , color : "92, 3, 224"},
-    {type : "difficultLevel" , filtering : "Epic" , color : "157, 45, 45"},
-    {type : "difficultLevel" , filtering : "Unique" , color : "35, 176, 40"},
-    {type : "difficultLevel" , filtering : "Default" , color : "222, 222, 222"},
+    {type : "difficultLevel" , filtering : null , color : "249, 228, 0"},
+    {type : "difficultLevel" , filtering : null , color : "92, 3, 224"},
+    {type : "difficultLevel" , filtering : null , color : "157, 45, 45"},
+    {type : "difficultLevel" , filtering : null , color : "35, 176, 40"},
+    {type : "difficultLevel" , filtering : null , color : "222, 222, 222"},
     {type : "status" , filtering: "complided" , color : "222, 222, 222"},
     {type : "status" , filtering : "rejected" , color : "222, 222, 222"},
     {type : "status" , filtering : "acepded" , color : "222, 222, 222"},
 ]
 
 const AchivemetDifficultItems = () => {
-    const [actived , setActived] = useState<Filtering[]>([])
+    const [filters , setFilters] = useState<(number)[]>([])
+
+    const choseFilter = (newFilter : number) => {
+        console.log(filters)
+        if(filters.includes(newFilter)) {
+            setFilters(prevFiltes => prevFiltes.filter(val => val !== newFilter) )
+        }
+        else {
+            setFilters(prevFilters => [...filters, newFilter])
+        }
+    }
 
     return (
         <div className = {styles.filterItems}>
             {
                 options.map(({type , filtering , color} , i ) => (
                     <div 
-                    key={type + filtering + color} 
+                    key={i} 
                     className = {styles.filterItem} 
+                    onClick={() => choseFilter(i)}
                     style = {{backgroundColor : `rgb(${color})`}}>
-                        <img src={type == "status" ? `${filtering}.png` : ``} alt="" />
+
+                    {
+                        type == "status" ? <img src={`${filtering}.png`}/> : null
+                    }
+
                     </div>
                 ))
             }
